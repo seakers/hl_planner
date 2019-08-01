ff50755eae3090f56ee8cf1a7b731e35ce09d984
Antoni Viros <ani300@gmail.com>
2019-07-31 20:43:01 -0700

    First steps in making the system standalone

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ from spinup import ppo
 import tensorflow as tf
 import gym
 
-env_fn = lambda : gym.make('gym_orekit:orekit-v0')
 
-ac_kwargs = dict(hidden_sizes=[64, 64], activation=tf.nn.relu)
+def main():
+    args = {
+        "forest_data_path": "/Users/anmartin/Projects/summer_project/hl_planner/forest_data.tiff",
+        "num_measurements": 6,
+        "max_forest_heights": [60, 90, 45, 38, 30, 76],
+        "orbit_altitude": 757000,
+    }
+    env_fn = lambda: gym.make('gym_orekit:online-orekit-v0', **args)
 
-logger_kwargs = dict(output_dir='./output', exp_name='test1')
+    ac_kwargs = dict(hidden_sizes=[64, 64], activation=tf.nn.relu)
+    logger_kwargs = dict(output_dir='./output', exp_name='test1')
+    ppo(env_fn=env_fn, ac_kwargs=ac_kwargs, steps_per_epoch=20160, epochs=10, max_ep_len=20160, save_freq=2, logger_kwargs=logger_kwargs)
 
-ppo(env_fn=env_fn, ac_kwargs=ac_kwargs, steps_per_epoch=20160, epochs=10, max_ep_len=20160, save_freq=2, logger_kwargs=logger_kwargs)
+
+if __name__ == '__main__':
+    main()
