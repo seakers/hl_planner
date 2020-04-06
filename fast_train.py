6f72b17d3632ae8b15c45fcebdd791a6b710cffd
Antoni Viros Martin <ani300@gmail.com>
2020-04-06 12:05:14 -0500

    Updated ray algorithm

--- fast_train.py
+++ fast_train.py
@@ -1,3 +1,7 @@
+#from stable_baselines.common.policies import MlpPolicy
+#from stable_baselines.common import make_vec_env
+#from stable_baselines import PPO2
+import os
 import ray
 from ray import tune
 
@@ -5,14 +9,22 @@ from gym_orekit.envs import OfflineOrekitEnv
 
 
 def main():
+    curdir = os.getcwd()
     args = {
-        "forest_data_path": "/Users/anmartin/Projects/summer_project/hl_planner/forest_data.tiff",
-        "simulation_data_path": "/Users/anmartin/Projects/FormationSimulation/fastsimulation.json",
+        "forest_data_path": os.path.join(curdir, "forest_data.tiff"),
+        "simulation_data_path": os.path.join(curdir, "fastsimulation.json"),
         "num_measurements": 6,
         "max_forest_heights": [60, 90, 45, 38, 30, 76],
         "orbit_altitude": 757000,
         "draw_plot": False
     }
+
+    # multiprocess environment
+    # env = make_vec_env(OfflineOrekitEnv, n_envs=8, env_kwargs=args)
+    #
+    # model = PPO2(MlpPolicy, env, verbose=1, n_steps=20162, nminibatches=8, cliprange=10000, tensorboard_log="./log_1/")
+    # model.learn(total_timesteps=20162*3000)
+    # model.save("plan_ppo")
 
     ray.init()
     tune.run(
@@ -22,7 +34,7 @@ def main():
         },
         config={
             "env": OfflineOrekitEnv,
-            "num_workers": 3,
+            "num_workers": 4,
             #"lr": tune.grid_search([0.01, 0.001, 0.0001]),
             "batch_mode": "complete_episodes",
             "env_config": args,
@@ -30,8 +42,9 @@ def main():
             # Size of batches collected from each worker
             "sample_batch_size": 20162,
             # Number of timesteps collected for each SGD round
-            "train_batch_size": 60486,
-            "vf_clip_param": 5000
+            "train_batch_size": 80648,
+            "vf_clip_param": 5000,
+            "num_gpus": 1
         },
         checkpoint_freq=100
     )
